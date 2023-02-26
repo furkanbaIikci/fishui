9f8825521f053d4903074cd9dfbae51e5165fc07
Furkan <furkan_blkc@hotmail.com>
2023-02-26 16:29:03 +0300

    created buttons

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
 
-import { ExampleComponent } from 'fishui'
+import { Button, ExampleComponent } from 'fishui'
 import 'fishui/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      
+      <ExampleComponent text='Create React Library Example 😄' />
+      <Button type='primary' text='Primary Button' />
+      <Button type='secondary' text='Secondary Button' />
+      <Button type='danger' text='Danger Button' />
+      <Button type='warn' text='Warning Button' />
+
+      
+    </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ import styles from './styles.module.css'
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
+
+
+export const Button = ({type, text}) => {
+
+  const onClickHandler = (type) => {
+    alert(`You clicked on ${type} button`)
+  }
+  return(
+    <button onClick={() => onClickHandler(type)} className={styles[type]}>{text}</button>
+  )
+
+}
